b719cf9dc999d0d4a00e6d9f5148829a055c742b
Tarun Mittal <tarunmittal2012@rediffmail.com>
2019-04-16 00:28:34 +0530

    Update  Data
    Signed-off-by: Tarun Mittal <tarunmittal2012@rediffmail.com>

--- app/src/main/java/com/example/tarunmittal/flighttracker/Interface/ApiInterface.java
+++ app/src/main/java/com/example/tarunmittal/flighttracker/Interface/ApiInterface.java
@@ -1,25 +1,22 @@
 package com.example.tarunmittal.flighttracker.Interface;
 
-import com.example.tarunmittal.flighttracker.Model.Arrival;
-import com.example.tarunmittal.flighttracker.Model.Data;
-import com.example.tarunmittal.flighttracker.Model.Departure;
 import com.example.tarunmittal.flighttracker.Model.Example;
-import com.example.tarunmittal.flighttracker.Model.Flight;
 
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 public interface ApiInterface {
 
-    String BASE_URL="http://aviation-edge.com/v2/public";
+    String BASE_URL = "http://aviation-edge.com/v2/public";
     @GET("flights?key=9568a0-8ff8d3")
-    Call<Data>getDepartureFlights(
-                                           @Query("deplata")String deptString);
+    Call<List<Example>> getDepartureFlights(
+            @Query("depIata") String deptString);
     @GET("flights?key=9568a0-8ff8d3")
-    Call<Data>getArrivalFlights(
-                                         @Query("arrIata")String arrivalString);
+    Call<List<Example>> getArrivalFlights(
+            @Query("arrIata") String arrivalString);
+    @GET("flights?key=9568a0-8ff8d3")
+    Call<List<Example>> getFlight(@Query("flightNum") String flightNum);
+
 }
--- app/src/main/java/com/example/tarunmittal/flighttracker/MainActivity.java
+++ app/src/main/java/com/example/tarunmittal/flighttracker/MainActivity.java
@@ -2,22 +2,20 @@ package com.example.tarunmittal.flighttracker;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.tarunmittal.flighttracker.Adapter.ArrivalListAdapter;
 import com.example.tarunmittal.flighttracker.ApiClient.ApiClient;
 import com.example.tarunmittal.flighttracker.Interface.ApiInterface;
-import com.example.tarunmittal.flighttracker.Model.Arrival;
-import com.example.tarunmittal.flighttracker.Model.Data;
+import com.example.tarunmittal.flighttracker.Model.Departure;
 import com.example.tarunmittal.flighttracker.Model.Example;
 
 import java.util.List;
@@ -29,9 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     ArrivalListAdapter mArrivalListAdapter;
 
-    EditText flightText;
+    AutoCompleteTextView flightText;
 
-    Button submit;
+    EditText flightNumber;
+
+    Button submit, search;
 
     ProgressDialog progressDoalog;
 
@@ -39,11 +39,16 @@ public class MainActivity extends AppCompatActivity {
 
     ApiInterface mApiInterface;
 
+    List<Example> mExampleList;
+
+    List<Example> getAllFlights;
+
+    ListView mView;
+
     private String TAG = MainActivity.class.toString();
 
     private String api_key = "9568a0-8ff8d3";
 
-    TextView mView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -51,8 +56,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         flightText = findViewById(R.id.edittext);
         submit = findViewById(R.id.submit);
-        mView=findViewById(R.id.flight_data);
+        mView = findViewById(R.id.flight_data);
+        search = findViewById(R.id.search);
+        flightNumber = findViewById(R.id.flight_text);
+        search.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+
+                progressDoalog = new ProgressDialog(MainActivity.this);
+                progressDoalog.setMessage("Please Wait..");
+                progressDoalog.setTitle("Loading!");
+                progressDoalog.show();
+                mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+                Call<List<Example>> call = mApiInterface.getFlight(flightNumber.getText().toString());
+                call.enqueue(new Callback<List<Example>>() {
+
+                    @Override
+                    public void onResponse(Call<List<Example>> call, Response<List<Example>> response) {
+
+                        progressDoalog.dismiss();
+                        getAllFlights = response.body();
+                        String[] code = new String[getAllFlights.size()];
+
+                        code[0] = getAllFlights.get(0).getDeparture().getIataCode();
+                        mView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, code));
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Example>> call, Throwable t) {
+
+                        progressDoalog.dismiss();
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
         submit.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -63,42 +102,37 @@ public class MainActivity extends AppCompatActivity {
                 progressDoalog.setTitle("Loading!");
                 progressDoalog.show();
                 mApiInterface = ApiClient.getClient().create(ApiInterface.class);
-                Call<Data> call = mApiInterface.getArrivalFlights(flightText.getText().toString());
+                Call<List<Example>> call = mApiInterface.getArrivalFlights(flightText.getText().toString());
+                call.enqueue(new Callback<List<Example>>() {
 
-                call.enqueue(new Callback<Data>() {
                     @Override
-                    public void onResponse(@NonNull Call<Data> call, @NonNull Response<Data> response) {
+                    public void onResponse(Call<List<Example>> call, Response<List<Example>> response) {
 
                         progressDoalog.dismiss();
-                        String display="";
-                        Data data=response.body();
-                        List<Arrival> arrivalList=data.arrivalList;
-                        for(Arrival arrival:arrivalList)
-                        {
-                            String a=arrival.getIataCode();
-                            String b=arrival.getIcaoCode();
-                            display =a+b;
+                        String s = "";
+                        mExampleList = response.body();
+                        Departure[] examples = new Departure[mExampleList.size()];
+                        String[] code = new String[mExampleList.size()];
+
+                        for (int i = 0; i < mExampleList.size(); i++) {
+                            examples[i] = mExampleList.get(i).getDeparture();
+                            code[i] = mExampleList.get(i).getDeparture().getIataCode();
                         }
-                        mView.setText(display);
-                     }
+                        mView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, code));
+                        int size = mExampleList.size();
+                        String n = Integer.toString(size);
+
+                    }
 
                     @Override
-                    public void onFailure(@NonNull Call<Data> call, @NonNull Throwable t) {
+                    public void onFailure(Call<List<Example>> call, Throwable t) {
 
                         progressDoalog.dismiss();
-                        Toast.makeText(MainActivity.this, "Error in Parsing", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
         });
-    }
-
-    private void getArrivalFlights(List<Arrival> arrivalList) {
-
-        mArrivalListAdapter = new ArrivalListAdapter(this, arrivalList);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-        flightList.setLayoutManager(layoutManager);
-        flightList.setAdapter(mArrivalListAdapter);
     }
 
 }
